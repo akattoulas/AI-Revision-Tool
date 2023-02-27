dd53979139f6ce0452d5d3d1177e45098b035fc9
Ola <okoczar@gmail.com>
2023-02-27 20:07:24 +0000

    working on prompt engineering

--- test.py
+++
@@ -1,8 +0,0 @@
-from secret_keys import OPENAI_API_KEY
-import openai
-
-openai.api_key = OPENAI_API_KEY
-openai.Model.list()
-
-#res = openai.File.create(file=open("testFile.jsonl"), purpose="classifications")
-#print(res)
